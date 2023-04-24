f0b8e9c667b1af0ba989280819eb79cc1f01f24a
Bindholt <Cbindholt@gmail.com>
2023-04-24 17:28:21 +0200

    Cleaned up unused code

--- login.js
+++ login.js
@@ -11,9 +11,9 @@ function main() {
 async function login(event) {
     event.preventDefault();
     const userName = document.querySelector("#user_name").value;
-    const pin = document.querySelector("#pin").value;
-    console.log(userName);
+
     const response = await fetch(`${endpoint}/users/${userName}.json`);
+    
     const data = await response.json();
 
     if(response.ok && await validPin(data.pin)) { 
