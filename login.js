94b512d284c81675a727b5087d02c058c84b4713
Bindholt <Cbindholt@gmail.com>
2023-04-24 16:18:26 +0200

    Created initial login page

---
+++ login.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.addEventListener("load", main);
+
+function main() {
+    document.querySelector("#btn_submit").addEventListener("mouseup", login);
+}
+
+async function login() {
+    
+}
