fd22526d9c26503eaaa4a0a4e4b6a292c1b0170d
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-29 08:44:55 +0200

    invite code done

--- addusertogroup.js
+++ addusertogroup.js
@@ -2,6 +2,32 @@
 
 window.addEventListener("load", main)
 
+const displaypasswordHtml = document.getElementById("password_generated");
+
 function main() {
     console.log("Det kører");
+
+    document.querySelector("#auto_password").addEventListener("click", displayPassword);
+
 }
+
+
+function autoGPassword() {
+    let pass = ""
+
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    for (let i = 1; i <= 10; i++) {
+        let generate = Math.floor(Math.random() * str.length + 1 );
+
+        pass+= str.charAt(generate)
+    }
+    console.log(pass);
+    return pass;
+    
+}
+
+const displayPassword = () => {
+    displaypasswordHtml.innerHTML = autoGPassword();
+}
+
