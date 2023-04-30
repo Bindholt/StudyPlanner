36ffbe43ed1c21f0ac5a8b51f1176087fc3728f2
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-04-30 13:59:58 +0200

    add user to group done

--- addusertogroup.js
+++ addusertogroup.js
@@ -3,13 +3,81 @@
 window.addEventListener("load", main)
 
 const displaypasswordHtml = document.getElementById("password_generated");
+const endpoint = "https://test-studygroup-default-rtdb.europe-west1.firebasedatabase.app/";
+const user = localStorage.getItem("userName");  
 
 function main() {
     console.log("Det kører");
 
     document.querySelector("#auto_password").addEventListener("click", displayPassword);
+    document.querySelector("#add_user_to_group").addEventListener("submit", addUserInG);
 
 }
+
+async function addUserInG(event) {
+    event.preventDefault();
+
+    const groupName = await findGroupName(event.target["invite_code"].value);
+    
+    const amount = await findAmountOfGroupMembers(groupName);
+
+    await insertMemberInGroup(amount, groupName);
+
+
+
+
+
+
+}
+
+async function insertMemberInGroup(amount, groupName) {
+        const userData = {
+            [amount]: user
+        }
+        
+        const postAsJson = JSON.stringify(userData);
+        console.log(postAsJson);
+      
+      const response = await fetch(`${endpoint}/group/${groupName}/members.json`, {
+        method: "PATCH",
+        body: postAsJson,
+      });
+      if (response.ok) {
+        console.log("Members igennem");
+      }
+
+
+}
+
+async function findGroupName(inviteCode) {
+     const response = await fetch(`${endpoint}/inviteCodes/${inviteCode}.json`);
+     const data = await response.json();
+
+     if (data !== null) {
+        return data.groupName
+     } else {
+        return showErrorM();
+     }
+}
+
+async function findAmountOfGroupMembers(groupName) {
+    const members = await fetch(`${endpoint}/group/${groupName}/members.json`);
+    const membersObject = await members.json(); 
+
+    let i = 0;
+    for (let member in membersObject) {
+      i++;
+    }
+
+    console.log(i);
+
+    return i;
+
+
+
+
+}
+
 
 
 function autoGPassword() {
@@ -34,11 +102,15 @@ const displayPassword = () => {
 
 
 function createRandomPins() {
-  const pins = document.querySelectorAll('[name="pin"]');
+  
 
-  for (let i = 0; i < pins.length; i++) {
+  
     const pin = (Math.floor(Math.random() * 10000000) + 10000000).toString().substring(1);
-    document.querySelector(`label[for="${pins[i].id}"`).innerHTML = pin;
-    pins[i].value = pin;
-  }
+    
+   return pin 
+}
+
+
+function showErrorM() {
+    console.log("Error");
 }
