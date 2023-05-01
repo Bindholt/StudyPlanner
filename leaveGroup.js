2dfb385df9bdf8220aea0f24321a50624e373f63
Bindholt <Cbindholt@gmail.com>
2023-05-01 14:43:55 +0200

    idk

--- leaveGroup.js
+++ leaveGroup.js
@@ -3,7 +3,23 @@
 const endpoint = "https://studyplanner-ad697-default-rtdb.europe-west1.firebasedatabase.app/"; 
 
 window.addEventListener("load", main);
+const user = localStorage.getItem("userName");
+const group = localStorage.getItem("groupName");
 
-function main() {
-    
+async function main() {
+    setGroupNameHTML();
+    setEventListener();
+}
+
+function setGroupNameHTML() {
+    document.querySelector("h1").innerHTML = group;
+}
+
+function setEventListener() {
+    document.querySelector("#btn_leave").addEventListener("mouseup", leaveGroup);
+}
+
+async function leaveGroup() {
+    const groupFetch = await fetch(`${endpoint}/group/${group}/members/2.json`);
+    console.log(groupFetch.json());
 }
