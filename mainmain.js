6ce055a9430cf287d8392e43c809a85fdff0add4
Bindholt <Cbindholt@gmail.com>
2023-05-03 13:14:46 +0200

    Added sorting to members, handling of whether you're in a group or not and some cleanup

--- mainmain.js
+++ mainmain.js
@@ -3,15 +3,33 @@
 window.addEventListener("load", main);
 const endpoint = "https://studyplanner-ad697-default-rtdb.europe-west1.firebasedatabase.app/"; 
 const group = localStorage.getItem("groupName");
-const memberArray = [];
+let memberArray = [];
 
 async function main() {
+  setUserNameHTML();
   setEventListeners();
+  
   if(group && group.length > 0) {
     handleGroupHTML();
+    hasGroupHideLinks();
   } else {
-
+    noGroupHideLinks();
   }
+}
+
+function hasGroupHideLinks() {
+  document.querySelector("#add_user_to_group").style.display = "none";
+  document.querySelector("#create_group").style.display = "none";
+}
+
+function noGroupHideLinks() {
+  document.querySelector("#calendar").style.display = "none";
+  document.querySelector("#chat").style.display = "none";
+  document.querySelector("#leave_group").style.display = "none";
+}
+
+function setUserNameHTML() {
+  document.querySelector("#user_name").innerText = localStorage.getItem("userName");
 }
 
 function setEventListeners() {
@@ -20,15 +38,14 @@ function setEventListeners() {
   document.querySelector("#calendar").addEventListener("mouseup", () => window.location = "calendar.html");
   document.querySelector("#chat").addEventListener("mouseup", () => window.location = "chat.html");
   document.querySelector("#leave_group").addEventListener("mouseup", () => window.location = "leaveGroup.html");
+  document.querySelector("#change_user").addEventListener("mouseup", logout);
 }
-let test;
+
 async function handleGroupHTML() {
   const groupResponse = await getGroupInformation();
 
   if (groupResponse.ok) {
     const groupData = await groupResponse.json();
-    
-    test = groupData;
     setGroupHTML(groupData);
     
     for (const member in groupData.members) {
@@ -46,11 +63,38 @@ function setGroupHTML(groupData) {
   const groupHTML = /* html */ `
       <h2>Group: ${groupData.groupName}</h2>
       <h3>Invite code: ${groupData.inviteCode}</h3>
+      <label for="sort">Sort by</label>
+      <select id="sort" name="sort">
+        <option value=""></option>
+        <option value="first_name">First Name</option>
+        <option value="last_name">Last Name</option>
+      </select>
       <section id="members_container">
-        
+
       </section>
   `
   document.querySelector(".group_container").insertAdjacentHTML("afterbegin", groupHTML);
+  document.querySelector("#sort").addEventListener("change", event => sortMembers(event));
+}
+
+function sortMembers(event) {
+  switch (event.target.value) {
+    case "first_name":
+      memberArray = memberArray.sort((a,b) => a.fullName.split(" ")[0].localeCompare(b.fullName.split(" ")[0]));
+      break;
+    case "last_name":
+      memberArray = memberArray.sort((a,b) =>  a.fullName.split(" ")[a.fullName.split(" ").length -1].localeCompare(b.fullName.split(" ")[b.fullName.split(" ").length -1]));
+      break;
+    default:
+      return;
+  }
+
+  document.querySelector("#members_container").innerHTML = "";
+  for (const member in memberArray) {
+    setMemberHTML(memberArray[member]);
+  }
+
+
 }
 
 function setMemberHTML(member) {  
@@ -74,4 +118,11 @@ async function getMembersInformation(member) {
   const response = await fetch(`${endpoint}/users/${member}.json`);
 
   return response;
+}
+
+function logout() {
+  localStorage.removeItem("userName");
+  localStorage.removeItem("groupName");
+  
+  window.location = "/login.html";
 }
